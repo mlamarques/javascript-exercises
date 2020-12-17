1b8e6e239c5b152374d748ef695d4223ca0cfd8c
Mark Alves <lamarques.marcos@gmail.com>
2020-12-17 04:59:49 -0300

    repeatString solved

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(word, n) {
+    let ans = ''
+    if (n < 0) {
+        ans = 'ERROR'
+        return ans
+    } else {
+        for (let i = 1; i <= n; i++) {
+            if (n === 0) {
+                break
+            } else {
+                ans = ans.concat(word)
+            }
+        } return ans
+    }
 }
 
 module.exports = repeatString
