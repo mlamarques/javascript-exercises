ce4cbfbf2244ad8f4dcd136ef5c921c9be588094
Mark Alves <lamarques.marcos@gmail.com>
2020-12-17 16:56:14 -0300

    removeFromArray solved

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elem) {
+    for (let i = 0; i < elem.length; i++) {
+        if (array.indexOf(elem[i]) == -1) {
+            continue
+        } else {
+            let pos = array.indexOf(elem[i])
+            array.splice(pos, 1)
+        }
+    } return array
 }
 
 module.exports = removeFromArray
