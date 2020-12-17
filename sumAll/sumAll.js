9149409052df663b7f5645120bf8f05e71da3ef3
Mark Alves <lamarques.marcos@gmail.com>
2020-12-17 17:57:07 -0300

    sumAll solved

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    //allNumbers = []
+    count = 0
+    initialNum = Math.min(numOne, numTwo)
+    lastNum = Math.max(numOne, numTwo)
+    
+    if (numOne < 0 || numTwo < 0 || typeof numOne !== 'number' || typeof numTwo !== 'number') {
+        return "ERROR"
+    } else {
+        for (let  i = initialNum; i <= lastNum; i++) {
+            //allNumbers.push(i)
+            count = count + i
+        } return count
+    }
+    
 }
 
 module.exports = sumAll
