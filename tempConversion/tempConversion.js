415c1b90439f92be5f2f9e2f23df40831d505a82
Mark Alves <lamarques.marcos@gmail.com>
2020-12-18 05:05:29 -0300

    tempConversion solved

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let c = (f - 32) * 5 / 9
+  c = Number(c.toFixed(1))
+  return c
 }
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let f = (c * 9 / 5) + 32
+  f = Number(f.toFixed(1))
+  return f
 }
 
 module.exports = {
